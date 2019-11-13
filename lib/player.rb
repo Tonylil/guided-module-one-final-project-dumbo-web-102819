c5d71d806f443267b50c26362224e8248632766e
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-11-13 11:34:33 -0500

    added friend and trap rooms

--- lib/player.rb
+++ lib/player.rb
@@ -5,6 +5,13 @@ class Player < ActiveRecord::Base
 		self.hp -= amt
 	end
 
+	def get_heal(amt)
+		self.hp += amt
+		if self.hp > self.max_hp
+			self.hp = self.max_hp
+		end
+	end
+
 	#TODO: Check Death
 	#Check if current Hp is less than 0, return true if dead, false if alive
 	def death?
